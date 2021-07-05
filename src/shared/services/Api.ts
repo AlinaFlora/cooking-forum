7c97eed7c9c7e019eb836049a6a5352449ac3636
Alina <alina.flora@bcf-software.pl>
2021-07-05 09:58:17 +0200

    interceptor added

--- src/shared/services/Api.ts
+++ src/shared/services/Api.ts
@@ -1,11 +1,30 @@
 import axios, { AxiosInstance } from 'axios'
 import { config } from "../../config";
+import paths from "../../config/paths";
 
 class Api {
   public api: AxiosInstance
 
   constructor() {
     this.api = axios.create({ baseURL: config.API_URL })
+
+    this.api.interceptors.response.use(
+      response => {
+        if ( response.status === 201) {
+          alert("Success");
+        }
+        return response
+      },
+      error => {
+        if (error.response.status === 401) {
+          localStorage.clear()
+          window.location.replace(paths.forum)
+        } else {
+          return Promise.reject(error)
+        }
+      }
+    )
+
   }
 }
 
